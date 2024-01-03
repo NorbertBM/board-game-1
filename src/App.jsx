c8c3591adef75934870b754c41293d471c5b73a3
NorbertBM <menyhart.b.norbert@gmail.com>
2024-01-03 22:04:10 +0200

    Root

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { Routes, Route, Outlet, Link } from "react-router-dom";
-import Nav from "./components/nav";
+// import React, { useState, useEffect } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 
+import Root from "./routes/root";
 import Home from "./routes/home";
 import About from "./routes/about";
 import Dashboard from "./routes/dashboard";
+
 export default function App() {
   // responsive
-  const [isMobile, setIsMobile] = useState(
-    window.matchMedia("(max-width:600px)").matches
-  );
+  //   const [isMobile, setIsMobile] = useState(
+  //     window.matchMedia("(max-width:600px)").matches
+  //   );
 
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      setIsMobile(window.matchMedia("(max-width:600px)").matches);
-    });
-  }, []);
+  //   useEffect(() => {
+  //     window.addEventListener("resize", () => {
+  //       setIsMobile(window.matchMedia("(max-width:600px)").matches);
+  //     });
+  //   }, []);
 
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Root />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="dashboard" element={<Dashboard />} />
@@ -31,28 +32,6 @@ export default function App() {
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
-    </div>
-  );
-}
-
-function Layout() {
-  return (
-    <div>
-      <Nav />
-      <hr />
-      <h1>
-        Welcome to <span className="game-name">"Oneiros"</span> Board Game
-      </h1>
-      {/* createa a hover over the word "Oneiros" when the user howers over the word a popup will apear with the descriptuon of the word  */}
-
-      <p>Oneiros is a Greek word meaning "dream".</p>
-      <p>In Greek mythology, Oneiros was the personification of dreams.</p>
-      <img
-        className="oneiros-img"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/James_Heath_-_Nestor_Appearing_in_a_Dream_to_Agamemnon%2C_1805.jpg/440px-James_Heath_-_Nestor_Appearing_in_a_Dream_to_Agamemnon%2C_1805.jpg"
-        alt=""
-      />
-      <Outlet />
     </div>
   );
 }
