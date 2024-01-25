37d732ee8033c22f12c54237fa76207fa6509660
NorbertBM <menyhart.b.norbert@gmail.com>
2024-01-25 11:44:34 +0100

    added abilities

--- src/components/cards/AbilityCard.js
+++ src/components/cards/AbilityCard.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { FaWpexplorer } from "react-icons/fa";
+import { GrVulnerability } from "react-icons/gr";
+// import { FaWpexplorer } from "react-icons/fa";
 // import { FaEye } from "react-icons/fa6";
 import { GiWingfoot } from "react-icons/gi";
 import "../../css/ability-card.css";
@@ -15,18 +16,18 @@ export default function AbilityCard({
     <div className={`ability-card  ${card_type}`}>
       <header className="ability-card-header">
         <p className="ability">
-          <FaWpexplorer size={50} />
+          <GrVulnerability size={35} />
           {/* {hp} */}
         </p>
         <h2 className="exploration">{card_name}</h2>
-        <p className="attack">
+        <p className="stamina">
           <GiWingfoot size={35} />
           {stamina}
         </p>
       </header>
       <div className="img-container">
         <img src={img} alt="img" className="card-img" />
-        <div className="unit-parts"></div>
+        {/* <div className="unit-parts"></div> */}
       </div>
       {children}
     </div>
--- src/routes/guide_routes/abilities.js
+++ src/routes/guide_routes/abilities.js
@@ -1,39 +1,39 @@
 import React from "react";
 import AbilityCard from "../../components/cards/AbilityCard";
 import { GiInfinity } from "react-icons/gi";
+
+import { GrVulnerability } from "react-icons/gr";
 export default function Abilities() {
   return (
     <div className="sub-page">
       <h2>Abilities</h2>
-      <h4>Hide</h4> — this is another new and appealing mechanic and this is how
-      it goes. Hide uses ALL the stamina and makes the player “invisible” for
-      the mobs. You do however have to be at least two squares away from the mob
-      you’re trying to hide from (and not move of course). As stated, fears are
-      really hard to defeat, so a lot of the times you will be running or hiding
-      from them. However while the player is indeed hidden, the mob will move to
-      your last known location and you will have to roll a dice to see if the
-      mob sees you or not. If the mob does…good luck :D 6. Distract — another
-      new action, not sure how it works yet, but going with the idea that mobs
-      are hard to beat, you will be able to use items or sound to distract mobs
-      to different squares so that you can go around them. Again you have to be
-      a certain distance away from them, so you can’t distract a mob that is on
-      the same square as you. 7. Dash — characters can dash across two squares
-      for the price of 3 stamina. Example: normally you would pay 2 stamina to
-      walk across 2 squares (1 stamina per square). However players can choose
-      to dash across 2 squares for the price of 3 stamina. This ability will go
-      on cool down for a number of turns and if useful when avoiding combat.
-      <h4>Interact</h4>
       <p>
-        at this point this is mainly for characters interacting with each other
-        to trade items and such, but ideally we want them to be able to interact
-        with NPCs and perhaps even the world itself. Just not sure HOW a player
-        / character would interact with the world.
+        The ability cards ar market with the symbol <GrVulnerability />
       </p>
-      <h3>Enemy placement</h3>
-      <p>The placement of the enemy is determined by two factors</p>
+
       {/* create a infinity sign */}
-      <main className="main-container">
-        <section className="units-section lvl_1">
+      <main
+        className="main-container"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <section className="abilities-section hide">
+          <h4>Hide</h4>{" "}
+          <p>
+            this is another new and appealing mechanic and this is how it goes.
+            Hide uses ALL the stamina and makes the player “invisible” for the
+            mobs. You do however have to be at least two squares away from the
+            mob you’re trying to hide from (and not move of course). As stated,
+            fears are really hard to defeat, so a lot of the times you will be
+            running or hiding from them. However while the player is indeed
+            hidden, the mob will move to your last known location and you will
+            have to roll a dice to see if the mob sees you or not. If the mob
+            does…good luck :D
+          </p>
           <AbilityCard
             card_name={"Hide"}
             stamina={<GiInfinity />}
@@ -42,6 +42,65 @@ export default function Abilities() {
             <p className="description">
               Use all stamina to hide from enemies. Enemies will move to your
               last known location.
+            </p>
+          </AbilityCard>
+        </section>
+        <section className="abilities-section distract">
+          {" "}
+          <h4>Distract</h4>
+          <p>
+            {" "}
+            another new action, not sure how it works yet, but going with the
+            idea that mobs are hard to beat, you will be able to use items or
+            sound to distract mobs to different squares so that you can go
+            around them. Again you have to be a certain distance away from them,
+            so you can’t distract a mob that is on the same square as you. Can
+            not be used in combat.
+          </p>
+          <AbilityCard
+            card_name={"Distract"}
+            stamina={<GiInfinity />}
+            img={"/img/abilities/distract-2.jpg"}
+          >
+            <p className="description">
+              Move the enemy the equal amount of squares as your spend stamina.
+            </p>
+          </AbilityCard>
+        </section>
+        <section className="abilities-section dash">
+          <h4>Dash</h4>
+          <p>
+            — characters can dash across two squares for the price of 3 stamina.
+            Example: normally you would pay 2 stamina to walk across 2 squares
+            (1 stamina per square). However players can choose to dash across 2
+            squares for the price of 3 stamina. This ability will go on cool
+            down for a number of turns and if useful when avoiding combat.
+          </p>
+          <AbilityCard
+            card_name={"Dash"}
+            stamina={3}
+            img={"/img/abilities/dash-1.jpg"}
+          >
+            <p className="description">
+              Dash across two squares for the price of 3 stamina.
+            </p>
+          </AbilityCard>
+        </section>
+        <section className="abilities-section interact">
+          <h4>Interact</h4>
+          <p>
+            At this point this is mainly for characters interacting with each
+            other to trade items and such, but ideally we want them to be able
+            to interact with NPCs and perhaps even the world itself. Just not
+            sure HOW a player / character would interact with the world.
+          </p>
+          <AbilityCard
+            card_name={"Interact"}
+            stamina={1}
+            img={"/img/abilities/interact-1.jpg"}
+          >
+            <p className="description">
+              Interact with a character or NPCs for 1 stamina.
             </p>
           </AbilityCard>
         </section>
